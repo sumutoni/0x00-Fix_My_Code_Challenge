7a26873532cb2ac6b83aa9b1104e1c7807e30574
sumutoni <steumutoni@gmail.com>
2022-12-02 02:10:11 -0800

    fixed user

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         print("User.id should be unique")
 
     u_pwd = "myPassword"
-    user_1.password(u_pwd)
+    user_1.password = u_pwd
     if user_1.password == u_pwd:
         print("User.password should be hashed")
 
@@ -83,7 +83,6 @@ if __name__ == '__main__':
     user_2.password = 89
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
-
     if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
